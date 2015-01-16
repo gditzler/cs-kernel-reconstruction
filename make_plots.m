888cfe7edf5381d4a2c32323d7f5f0c47681ed51
Gregory Ditzler <gregory.ditzler@gmail.com>
2015-01-16 14:50:37 -0500

    updated plot script.

--- make_plots.m
+++ make_plots.m
@@ -8,11 +8,12 @@ load mat/gaussian_reconstruction_n20k7ka7.mat
 h = figure; 
 hold on;
 box on;
-plot(errs(2,:), 'ro-', 'LineWidth', 2);
-plot(errs(3,:), 'bs-', 'LineWidth', 2);
-plot(errs(4,:), 'k^-', 'LineWidth', 2);
-plot(errs(5,:), 'cp-', 'LineWidth', 2);
+plot(errs_no_norm(2,:), 'ro-', 'LineWidth', 2);
+plot(errs_no_norm(3,:), 'bs-', 'LineWidth', 2);
+plot(errs_no_norm(4,:), 'k^-', 'LineWidth', 2);
+plot(errs_no_norm(5,:), 'cp-', 'LineWidth', 2);
 xlim([1, M])
+ylim([0, 1.5*max(errs_no_norm(2,:))])
 legend('KR', 'CoSamp', 'OMP', 'L1KR','Location', 'best');
 xlabel('m', 'FontSize', 20);
 ylabel('reconstruction error', 'FontSize', 20);
@@ -21,7 +22,7 @@ set(gca, 'fontsize', 20);
 g = figure; 
 hold on;
 box on;
-plot(timez(2,:), 'ro-', 'LineWidth', 2);
+plot(timez(1,:), 'ro-', 'LineWidth', 2);
 plot(timez(3,:), 'bs-', 'LineWidth', 2);
 plot(timez(4,:), 'k^-', 'LineWidth', 2);
 plot(timez(5,:), 'cp-', 'LineWidth', 2);
