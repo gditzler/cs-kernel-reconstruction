df4e82270f69e50223c277a8cffff6fc1ee3f7a4
Gregory Ditzler <gregory.ditzler@gmail.com>
2015-02-17 12:24:41 -0500

    added  case

--- make_plots.m
+++ make_plots.m
@@ -3,27 +3,30 @@ clc;
 clear;
 close all;
 
-% load mat/gaussian_reconstruction_n20k7ka10.mat
-% load mat/gaussian_reconstruction_n20k7ka7.mat
-% load mat/gaussian_reconstruction_n25k7ka7.mat
+load mat/Gaussian_n20k7ka9.mat
+% load mat/GaussianShift_n20k7ka9.mat
 
-% load mat/Gaussian_n20k7ka10.mat
-% load mat/Gaussian_n20k7ka7.mat
-load mat/Gaussian_n20k7ka3.mat
-% load mat/GaussianShift_n20k7ka10.mat
-% load mat/GaussianShift_n20k7ka7.mat
-% load mat/GaussianShift_n20k7ka3.mat
+I = 2:size(errs_no_norm,2);
+
+% 1) kr (min error)
+% 2) kr (min sparse)
+% 3) cosamp
+% 4) omp
+% 5) l1kr
+% 6) l1
 
 h = figure; 
 hold on;
 box on;
-plot(errs_no_norm(2,:), 'ro-', 'LineWidth', 2);
-plot(errs_no_norm(3,:), 'bs-', 'LineWidth', 2);
-plot(errs_no_norm(4,:), 'k^-', 'LineWidth', 2);
-plot(errs_no_norm(5,:), 'cp-', 'LineWidth', 2);
+plot(I, errs_no_norm(2, I), 'ro-', 'LineWidth', 2);
+plot(I, errs_no_norm(3, I), 'bs-', 'LineWidth', 2);
+plot(I, errs_no_norm(4, I), 'k^-', 'LineWidth', 2);
+plot(I, errs_no_norm(5, I), 'cp-', 'LineWidth', 2);
+plot(I, errs_no_norm(6, I), 'ms-', 'LineWidth', 2);
 xlim([1, M])
-ylim([0, 1.5*max(errs_no_norm(2,:))])
-legend('KR', 'CoSamp', 'OMP', 'L1KR','Location', 'best');
+ylim([0, 1.5*max(errs_no_norm(2,I))])
+axis tight;
+legend('KR', 'CoSamp', 'OMP', 'L1KR', 'L1', 'Location', 'best');
 xlabel('m', 'FontSize', 20);
 ylabel('reconstruction error', 'FontSize', 20);
 set(gca, 'fontsize', 20);
@@ -32,27 +35,30 @@ saveas(h, ['eps/error_n',num2str(n),'k',num2str(k),'ka',num2str(k_alg),'.eps'], 
 g = figure; 
 hold on;
 box on;
-plot(timez(1,:), 'ro-', 'LineWidth', 2);
-plot(timez(3,:), 'bs-', 'LineWidth', 2);
-plot(timez(4,:), 'k^-', 'LineWidth', 2);
-plot(timez(5,:), 'cp-', 'LineWidth', 2);
+plot(I, timez(1, I), 'ro-', 'LineWidth', 2);
+plot(I, timez(3, I), 'bs-', 'LineWidth', 2);
+plot(I, timez(4, I), 'k^-', 'LineWidth', 2);
+plot(I, timez(5, I), 'cp-', 'LineWidth', 2);
 xlim([1, M])
-legend('KR', 'CoSamp', 'OMP', 'L1KR','Location', 'best');
+legend('KR', 'CoSamp', 'OMP', 'L1KR/L1', 'Location', 'best');
 xlabel('m', 'FontSize', 20);
 ylabel('time (s)', 'FontSize', 20);
+axis tight;
 set(gca, 'fontsize', 20);
 saveas(g, ['eps/times_n',num2str(n),'k',num2str(k),'ka',num2str(k_alg),'.eps'], 'eps2c')
 
 q = figure; 
 hold on;
 box on;
-plot(sparsity(2,:), 'ro-', 'LineWidth', 2);
-plot(sparsity(3,:), 'bs-', 'LineWidth', 2);
-plot(sparsity(4,:), 'k^-', 'LineWidth', 2);
-plot(sparsity(5,:), 'cp-', 'LineWidth', 2);
+plot(I, sparsity(2, I), 'ro-', 'LineWidth', 2);
+plot(I, sparsity(3, I), 'bs-', 'LineWidth', 2);
+plot(I, sparsity(4, I), 'k^-', 'LineWidth', 2);
+plot(I, sparsity(5, I), 'cp-', 'LineWidth', 2);
+plot(I, sparsity(6, I), 'ms-', 'LineWidth', 2);
 xlim([1, M])
-legend('KR', 'CoSamp', 'OMP', 'L1KR','Location', 'best');
+axis tight;
+set(gca, 'fontsize', 20);
+legend('KR', 'CoSamp', 'OMP', 'L1KR', 'L1', 'Location', 'best');
 xlabel('m', 'FontSize', 20);
 ylabel('sparsity', 'FontSize', 20);
-set(gca, 'fontsize', 20);
 saveas(q, ['eps/sparsity_n',num2str(n),'k',num2str(k),'ka',num2str(k_alg),'.eps'], 'eps2c')
--- src/l1_approximate_reconstruction.m
+++ src/l1_approximate_reconstruction.m
@@ -1,12 +1,15 @@
-function [x_kr, x_l1] = l1_approximate_reconstruction(A, y)
-%  x = l1_approximate_reconstruction(A, y)
+function [x_kr, x_l1] = l1_approximate_reconstruction(A, y, noise, delta)
+%  [x_kr, x_l1] = l1_approximate_reconstruction(A, y)
 % 
 %  INPUTS 
 %  @A: measurement matrix  
 %  @y: obeserved vector 
+%  @noise: use noise implementation [boolean] [optional]
+%  @delta: 
 %
 %  OUTPUTS
-%  @x: solution to the compressed system
+%  @x_kr: solution to the system using the l1-kr approx
+%  @x_l1: solution to the system using l1
 %
 %  AUTHORS
 %    Belhassan Bayer, Nidhal Bouynaya, Gregory Ditzler and Roman 
@@ -16,6 +19,13 @@ function [x_kr, x_l1] = l1_approximate_reconstruction(A, y)
 %
 %  LICENSE
 %    MIT
+if nargin == 2 
+  noise = false;
+  delta = 0;
+end
+if nargin == 3
+  delta = .001/sqrt(size(A,2));
+end
 X = null(A);
 s = size(X,2); % "s=dim(ker(A))"
 n = size(A,2);
@@ -28,10 +38,20 @@ cvx_begin quiet
 cvx_end
 
 x_l1 = x;
-x_kr = x;
-[~, i] = sort(abs(x));
 
-j = setdiff(1:n, i(1:s));
-xhat = inv(A(:, j))*y;
-x_kr = zeros(n,1);
-x_kr(j) = xhat;
+if noise == false
+  [~, i] = sort(abs(x));
+  j = setdiff(1:n, i(1:s));
+  xhat = inv(A(:, j))*y;
+  x_kr = zeros(n,1);
+  x_kr(j) = xhat;
+else
+  x_tmp = x;
+  x_tmp(abs(x_tmp) <= delta/sqrt(n)) = 0;
+  
+  [~, i] = sort(abs(x));
+  j = setdiff(1:n, i(1:s));
+  xhat = inv(A(:, j))*y;
+  x_kr = zeros(n,1);
+  x_kr(j) = xhat;
+end
