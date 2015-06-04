57c80c7a57120211411a10c789549369661f4366
Gregory Ditzler <gregory.ditzler@gmail.com>
2015-06-04 11:26:34 -0400

    updated the larger data set code

--- experiment_larger_data.m
+++ experiment_larger_data.m
@@ -17,14 +17,17 @@ set(0,'RecursionLimit', 5000);
 addpath('src/')
 
 n_avg = 250;
-n_set = 100:20:2000;
+n_set = [100:25:500 600:1000];
 k_set = floor(.05*n_set);
 M = 20;
 m = 30;
 k_alg_set = floor(.1*n_set);
 types = {'Gaussian', 'Uni',   'Bernoulli'};
-% delete(gcp('nocreate'));
-% parpool(50);
+opts.printEvery = 10000000;
+errFcn = [];
+  
+delete(gcp('nocreate'));
+parpool(20);
 
 for t = 1:length(types)
   errs = zeros(7, length(n_set));
@@ -32,8 +35,7 @@ for t = 1:length(types)
   timez = zeros(7, length(n_set));
   sparsity = zeros(7, length(n_set));
 
-  opts.printEvery = 10000000;
-  errFcn = [];
+  
 
   for i = 1:n_avg
     disp(['Running trial ',num2str(i), ' of ', num2str(n_avg)]);
@@ -50,8 +52,8 @@ for t = 1:length(types)
       tic;
       x_hat = cosamp(A, y, k_alg, errFcn, opts);
       timez(q, j) = timez(q, j) + toc;
-      errs(q, j) = errs(q, j) + per_error(x_hat/norm(x_hat), x/norm(x));
-      errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x_hat, x);
+      errs(q, j) = errs(q, j) + per_error(x/norm(x), x_hat/norm(x_hat));
+      errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x, x_hat);
       sparsity(q, j) = sparsity(q, j) + sum(abs(x_hat) >= sqrt(eps))/numel(x);
       q = q+1;
 
@@ -59,8 +61,8 @@ for t = 1:length(types)
       tic;
       x_omp = omp(A, y, k_alg, errFcn, opts);
       timez(q, j) = timez(q, j) + toc;
-      errs(q, j) = errs(q, j) + per_error(x_omp/norm(x_omp), x/norm(x));
-      errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x_omp, x);
+      errs(q, j) = errs(q, j) + per_error(x/norm(x), x_omp/norm(x_omp));
+      errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x, x_omp);
       sparsity(q, j) = sparsity(q, j) + sum(abs(x_omp) >= sqrt(eps))/numel(x);
       q = q+1;
 
@@ -68,18 +70,37 @@ for t = 1:length(types)
       tic;
       [x_l1kr, x_l1] = l1kr(A, y);
       timez(q, j) = timez(q, j) + toc;
-      errs(q, j) = errs(q, j) + per_error(x_l1kr/norm(x_l1kr), x/norm(x));
+      errs(q, j) = errs(q, j) + per_error(x/norm(x), x_l1kr/norm(x_l1kr));
       errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x_l1kr, x);
       sparsity(q, j) = sparsity(q, j) + sum(abs(x_l1kr) >= sqrt(eps))/numel(x);
       q = q+1;
       
-      errs(q, j) = errs(q, j) + per_error(x_l1/norm(x_l1), x/norm(x));
+      errs(q, j) = errs(q, j) + per_error(x/norm(x), x_l1/norm(x_l1));
+      errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x_l1, x);
+      sparsity(q, j) = sparsity(q, j) + sum(abs(x_l1) >= sqrt(eps))/numel(x);
+      q = q+1;
+      
+      % L1Noise-Approx of KR
+      tic;
+      [x_l1kr, x_l1] = l1kr_noise(A, y);
+      timez(q, j) = timez(q, j) + toc;
+      errs(q, j) = errs(q, j) + per_error(x/norm(x), x_l1kr/norm(x_l1kr));
+      errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x_l1kr, x);
+      sparsity(q, j) = sparsity(q, j) + sum(abs(x_l1kr) >= sqrt(eps))/numel(x);
+      q = q+1;
+      
+      errs(q, j) = errs(q, j) + per_error(x/norm(x), x_l1/norm(x_l1));
       errs_no_norm(q, j) = errs_no_norm(q, j) + per_error(x_l1, x);
       sparsity(q, j) = sparsity(q, j) + sum(abs(x_l1) >= sqrt(eps))/numel(x);
       
     end
     
   end
+  
+  errs = errs/n_avg;
+  errs_no_norm = errs_no_norm/n_avg;
+  sparsity = sparsity/n_avg;
+  timez = timez/n_avg;
   
   save(['mat/large_',types{t}, 'k', num2str(k), '.mat']);
 end
